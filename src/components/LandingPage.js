0d8895921032059291d060858b797f42aa952e44
DeathKrow <irfanhgazi@gmail.com>
2023-09-16 23:46:41 +0530

    Added registration page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+
 const LandingPage = () => {
   const navigate=useNavigate();
   return (
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,9 +1,77 @@
-import React from 'react'
+import React, { Component } from 'react';
+import '../App.css';
+import axios from 'axios';
 
-const SignUp = () => {
-  return (
-    <div>This is SignUp Screen</div>
-  )
+class SignUp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      password: ''
+    };
+  }
+
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSignUp = (e) => {
+    e.preventDefault();
+
+    const data = {
+      _id: "000001", // Create function to automatically assign id
+      Name: this.state.name,
+      Email: this.state.email,
+      Password: this.state.password
+    };
+
+    axios.post('http://localhost:8082/auth/register', data)
+    .then((res) => {
+      alert(res.data.title);
+      this.props.history.push('/home');
+    })
+    .catch((err) => {
+      alert(err);
+      this.props.history.push('/home');
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <form noValidate onSubmit={this.onSignUp}>
+          <input
+            type="text"
+            placeholder="Enter name"
+            name='name'
+            value = {this.state.name}
+            onChange = {this.onChange}
+            required
+          />         
+          <input
+            type="text"
+            placeholder="Enter email"
+            name='email'
+            value = {this.state.email}
+            onChange = {this.onChange}
+            required
+          />
+          <input
+            type="text"
+            placeholder="Enter password"
+            name='password'
+            value = {this.state.password}
+            onChange = {this.onChange}
+            required
+          />
+          <input
+            type="submit"
+          />  
+        </form>
+      </div>
+    )
+  }
 }
 
 export default SignUp
