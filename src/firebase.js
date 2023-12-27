44a56078059a46607b6aea2f584ef542f5f608a7
SoumyadiptoPal <soumyadiptocodes@gmail.com>
2023-12-27 12:41:12 +0530

    Updated Code for online hosting

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_TOKEN);
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
